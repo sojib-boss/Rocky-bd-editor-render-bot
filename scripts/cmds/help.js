ef5f74d0d1b239d9c4e9e39e6ae4075b858406fe
Sexy-Rocky <maxhusaedbot320@gmail.com>
2025-07-07 00:38:47 +0600

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -62,7 +62,7 @@ module.exports = {
       const totalCommands = commands.size;
       msg += `\n\n╭─────⭔[ 𝗘𝗻𝗷𝗼𝘆 🍀 ]\n│> 𝗧𝗼𝘁𝗮𝗹 𝗰𝗺𝗱𝘀: [${totalCommands}].\n│𝗧𝘆𝗽𝗲: [ ${prefix}𝗵𝗲𝗹𝗽 𝘁𝗼 \n│<𝗰𝗺𝗱> 𝘁𝗼 𝗹𝗲𝗮𝗿𝗻 𝘁𝗵𝗲 𝘂𝘀𝗮𝗴𝗲.]\n╰────────────:)`;
       msg += ``;
-      msg += `\n╭─────⭔\n│💫 | [𝐒𝐡𝐢𝐏𝐮 𝐀𝐢 🤖💨]\n╰────────────:-)`; // its not decoy so change it if you want 
+      msg += `\n╭─────⭔\n│💫 | [Hi Guys I Am ♡YOUR BABY♡ MY OWNER LINK m.me/Bangladesh.Top.Video.Editor.Rocky.Vai.320]\n╰────────────:-)`; // its not decoy so change it if you want 
 
 
       await message.reply({
